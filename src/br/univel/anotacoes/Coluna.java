f8c34ba0faace8ffb175f48975f896b9db4fc7d4
Valdecio Vinicius Plakitquen <valvplakitquen@hotmail.com>
2016-04-20 21:35:05 -0300

    Tentando iniciar

---
+++ src/br/univel/anotacoes/Coluna.java
@@ -0,0 +1,16 @@
+package br.univel.anotacoes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Coluna {
+
+	String nome() default "";
+
+	boolean pk() default false;
+
+}
